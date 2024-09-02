5d8ae8fbbf49bee28bc003e715097a46108833b2
mirza2018 <mirza.eee.4th@gmail.com>
2024-09-02 11:22:48 +0600

    footer add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navber from "@/components/Shared/Navber";
+import Footer from "@/components/Shared/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({ children }) {
         
         {children}
         
-        
+        <Footer/>
         
         </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Homepage from "@/components/Homepages/Homepage";
+
 
 export default function Home() {
   return (
     <main className="">
-
+<Homepage></Homepage>
     </main>
   );
 }
