5e3daa0c663dca35bf6b8a769aec1eef7c28b2a0
mirza2018 <mirza.eee.4th@gmail.com>
2024-09-02 11:13:45 +0600

    Navber add

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
- <div className="mockup-phone border-primary">
-  <div className="camera"></div>
-  <div className="display">
-    <div className="artboard artboard-demo phone-1">Hi.</div>
-  </div>
-</div>
+    <main className="">
+
     </main>
   );
 }
