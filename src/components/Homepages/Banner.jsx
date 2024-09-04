cc03ea622704fe4978d81e95cd88a9ed59070e5f
mirza2018 <mirza.eee.4th@gmail.com>
2024-09-04 10:45:20 +0600

    Slide add

--- src/components/Homepages/Banner.jsx
+++ src/components/Homepages/Banner.jsx
@@ -1,95 +1,85 @@
+"use client"
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Banner = () => {
+  const[slide,setSlide]=useState(slideData[0])
+  // console.log(slideData[2]);
+  const goNext=(id,add=1)=>{
+if(id==6){
+ return  setSlide(slideData[0])
+}
+  setSlide(slideData[id])
+
+
+
+  }
+
+console.log(slide);
+
+  
+  
     return (
 <div className="carousel w-full ">
 
-  <div id="slide1" className="carousel-item relative w-full">
 
 
+  <div  className="carousel-item relative w-full">
 <Image
     alt='slide'
     width={2500}
     height={1500}
 
-      src="https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-1.jpg"
+      src={slide.imag}
       className="w-full" />
 
-
-<Image
-    alt='slide'
-    width={2500}
-    height={1500}
-
-      src="https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-6.jpg"
-      className="w-full" />
-
-
     <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide2" className="btn btn-circle">❮</a>
-      <a href="#slide2" className="btn btn-circle">❯</a>
+      <a onClick={()=>goNext(slide.id)} className="btn btn-circle">❮</a>
+      <a onClick={()=>goNext(slide.id,-1)}  className="btn btn-circle">❯</a>
     </div>
   </div>
-  <div id="slide2" className="carousel-item relative w-full">
-   
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide1" className="btn btn-circle">❮</a>
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
+
+
+
+</div>
+
 
   
-</div>
+
     );
 };
 
 export default Banner;
 
 const slideData=[
-  {
-    imag:"http://websmirno.site/viktor/plumbio/images/mainslide-01__img.webp",
-    pre:"",
-    next:""
-  },
-  {
+  { id:1,
     imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-1.jpg",
+    pre:"1",
+    next:""
+  },
+  { id:2,
+    imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-5.jpg",
     pre:"",
     next:""
   },
-  {
+  { id:3,
     imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-2.jpg",
     pre:"",
     next:""
   },
-  {
+  { id:4,
     imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-3.jpg",
     pre:"",
     next:""
   },
-  {
+  { id:5,
     imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-6.jpg",
     pre:"",
     next:""
   },
-  {
-    imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-6.jpg",
+  { id:6,
+    imag:"https://plumbing-repair.themerex.net/wp-content/uploads/revslider/slider-1/178_1-1x-copyright-scaled.jpg",
     pre:"",
     next:""
-  },
-  {
-    imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-6.jpg",
-    pre:"",
-    next:""
-  },
-  {
-    imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-6.jpg",
-    pre:"",
-    next:""
-  },
-  {
-    imag:"https://airtech.bolvosites.com/wp-content/uploads/2019/09/slider-6.jpg",
-    pre:"",
-    next:""
-  },
+  }
 ]
