d4ca85be428d500fa8c3a926355f36a5f0db728c
mirza2018 <mirza.eee.4th@gmail.com>
2024-09-07 15:59:08 +0600

    Services of ccompany

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,6 @@
 import CeoSection from '@/components/AboutUS/CeoSection';
 import JoinUsTeam from '@/components/AboutUS/JoinUsTeam';
-import OurTeam from '@/components/AboutUS/OurTeam';
 import TeamMembers from '@/components/AboutUS/TeamMembers';
-import SelasNews from '@/components/Homepages/SelasNews';
 import ServiceTakeCompany from '@/components/Homepages/ServiceTakeCompany';
 import React from 'react';
 
@@ -10,9 +8,7 @@ const AboutPage = () => {
     return (
         <div>
             <CeoSection/>
-            {/* <OurTeam/> */}
             <TeamMembers/>
-            {/* <SelasNews/> */}
             <JoinUsTeam/>
             <ServiceTakeCompany/>
         </div>
--- src/components/Homepages/ServiceTakeCompany.jsx
+++ src/components/Homepages/ServiceTakeCompany.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 const ServiceTakeCompany = () => {
@@ -8,60 +9,62 @@ const ServiceTakeCompany = () => {
             <div className="lg:col-span-2">
                 <h2 className="text-3xl font-bold leading-tight text-gray-800 sm:text-4xl lg:text-5xl lg:leading-tight">
                     1 team.<br />
-                    6+ years.<br />
-                    300+ projects.<br />
+                    10+ years.<br />
+                    48000+ services Provide.<br />
                 </h2>
-                <p className="mt-6 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                <p className="mt-6 text-base text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ipsam magni quas corrupti! Quisquam perspiciatis, dolores recusandae obcaecati sequi, impedit quia, quod ducimus vero temporibus voluptas atque facere culpa. Exercitationem?</p>
             </div>
 
             <div className="lg:col-span-3 xl:col-span-4">
                 <div className="grid items-center max-w-4xl grid-cols-2 mx-auto lg:grid-cols-4 gap-x-10 gap-y-16">
                     <div>
-                        <img className="object-contain w-full h-6 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-1.png" alt="" />
+            
+
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-6 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-1.png" alt="" />
                     </div>
 
                     <div>
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-2.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-2.png" alt="" />
                     </div>
 
                     <div>
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-3.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-3.png" alt="" />
                     </div>
 
                     <div>
-                        <img className="object-contain w-full mx-auto h-7" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-4.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full mx-auto h-7" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-4.png" alt="" />
                     </div>
 
                     <div className="hidden lg:block">
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-5.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-5.png" alt="" />
                     </div>
 
                     <div className="hidden lg:block">
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-6.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-6.png" alt="" />
                     </div>
 
                     <div className="hidden lg:block">
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-7.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-7.png" alt="" />
                     </div>
 
                     <div className="hidden lg:block">
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-8.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-8.png" alt="" />
                     </div>
 
                     <div className="hidden lg:block">
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-9.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-9.png" alt="" />
                     </div>
 
                     <div className="hidden lg:block">
-                        <img className="object-contain w-full mx-auto h-7" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-10.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full mx-auto h-7" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-10.png" alt="" />
                     </div>
 
                     <div className="hidden lg:block">
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-11.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-11.png" alt="" />
                     </div>
 
                     <div className="hidden lg:block">
-                        <img className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-12.png" alt="" />
+                       <Image  placeholder='blur' blurDataURL='/loadingImg.gif' width={100} height={100}className="object-contain w-full h-8 mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/logos/3/logo-12.png" alt="" />
                     </div>
                 </div>
 
